c140edd391876bd35f5254ea3d3e7fe60d612d22
João Paulo <joaopaulo.data@gmail.com>
2022-08-22 11:52:36 -0300

    docs: 📑  Limpando notificação automaticamente

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
-import { INotificacao, TipoNotificacao } from "@/interfaces/INotificacao"
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { INotificacao } from "@/interfaces/INotificacao"
 
 interface Estado {
     projetos: IProjeto[],
@@ -14,26 +14,7 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
     state: {
         projetos: [],
-        notificacoes: [
-            {
-                id: 1,
-                texto: 'Uma notificação de sucesso',
-                titulo: 'sucesso',
-                tipo: TipoNotificacao.SUCESSO
-            },
-            {
-                id: 2,
-                texto: 'Uma notificação de atenção',
-                titulo: 'atenção',
-                tipo: TipoNotificacao.ATENCAO
-            },
-            {
-                id: 3,
-                texto: 'Uma notificação de falha',
-                titulo: 'falha',
-                tipo: TipoNotificacao.FALHA
-            },
-        ]
+        notificacoes: []
     },
     mutations: {
         [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
@@ -47,8 +28,15 @@ export const store = createStore<Estado>({
             const index = state.projetos.findIndex(proj => proj.id == projeto.id)
             state.projetos[index] = projeto
         },
-        [EXCLUIR_PROJETO](state, id: string) {
+        [EXCLUIR_PROJETO] (state, id: string) {
             state.projetos = state.projetos.filter(proj => proj.id !=id)
+        },
+        [NOTIFICAR] (state, novaNotificacao: INotificacao) {
+            novaNotificacao.id = new Date().getTime()
+            state.notificacoes.push(novaNotificacao)
+
+            setTimeout(()=> {
+                state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
         }
     }
 })
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -20,7 +20,10 @@
 <script lang="ts">
 import { useStore } from '@/store';
 import { defineComponent } from 'vue';
-import { ADICIONA_PROJETO, ALTERA_PROJETO } from '@/store/tipo-mutacoes';
+
+import { ADICIONA_PROJETO, ALTERA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes';
+import { TipoNotificacao } from '@/interfaces/INotificacao';
+
 export default defineComponent({
     name: 'Formulario',
     props: {
@@ -50,6 +53,11 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = "";
+            this.store.commit(NOTIFICAR, {
+                titulo: 'Novo projeto foi salvo',
+                texto: 'Prontinho ;) seu projeto já está disponível.',
+                tipo: TipoNotificacao.SUCESSO
+            })
             this.$router.push('/projetos')
         },
     },
